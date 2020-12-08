25f256fd7bbb232bdef3bd00ca8b7fbb3ecee5e3
Bianca Magalhães <bianca@12min.com>
2020-12-08 00:29:48 -0300

    WIP

--- src/main/java/br/com/yantra/springbootapi/repository/PortifolioRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.yantra.springbootapi.repository;
-
-import br.com.yantra.springbootapi.entity.Portifolio;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PortifolioRepository extends JpaRepository<Portifolio, Long> {
-}
